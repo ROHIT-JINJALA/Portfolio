945b15d1da8fae9a4811d419a78b6715cfe1232d
rohit <rohitjinjala50@gmail.com>
2025-11-17 10:42:49 +0530

    All fixed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 // eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
-import Section from "./section";
+import Section from "./Section";
 import { Github, Link } from "lucide-react";
 import projectData from "../data/ProjectData";
 
